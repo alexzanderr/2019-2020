b23d6f0d75a4d3234602e81a15bd294b120dac02
Alexzander <54772464+decoder45@users.noreply.github.com>
2020-01-04 02:17:33 +0200

    Add files via upload

---
+++ java_stuff/src/Useful/InstantiationException.java
@@ -0,0 +1,21 @@
+
+package Useful;
+
+public class InstantiationException extends Exception {
+
+    private String message;
+
+    public InstantiationException() {
+        this.message = "Information about the error is unknown.";
+    }
+
+    public InstantiationException(final String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        String result = String.format("<The error is '%s'>", this.message);
+        return result;
+    }
+}
